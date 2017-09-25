fcdeb89413c013d033620d87b56083e5f9fe22ef
Devon Stewart <dstewart@twilio.com>
2017-09-25 18:15:46 -0400

    Switching Target to be EitherT[WriterT[Id, List[String], ?], String, T]

--- modules/codegen/src/main/scala/com/twilio/swagger/codegen/package.scala
+++ modules/codegen/src/main/scala/com/twilio/swagger/codegen/package.scala
@@ -1,6 +1,9 @@
 package com.twilio.swagger
 
-import cats.data.Coproduct
+import cats.{Applicative, Id}
+import cats.data.{Coproduct, EitherT, WriterT}
+import cats.instances.all._
+import cats.syntax.applicative._
 import cats.syntax.either._
 import com.twilio.swagger.codegen.terms.ScalaTerm
 import com.twilio.swagger.codegen.terms.client.ClientTerm
@@ -12,19 +15,24 @@ package codegen {
   case class CodegenDefinitions(clients: List[Client], servers: List[Server], frameworkImports: List[Import])
 
   object Target {
-    type Type[A] = Either[String, A]
-    def pure[T](x: T): Target[T] = Either.right(x)
-    def error[T](x: String): Target[T] = Either.left(x)
-    def fromOption[T](x: Option[T], default: => String): Target[T] = Either.fromOption(x, default)
-    def unsafeExtract[T](x: Target[T]): T = x.valueOr({ err => throw new Exception(err.toString) })
+    type ErrorType = String
+    type Logger[T] = WriterT[Id, List[String], T]
+    type Type[A] = EitherT[Logger, ErrorType, A]
+    val A = Applicative[Target]
+    def pure[T](x: T): Target[T] = A.pure(x)
+    def error[T](x: ErrorType): Target[T] = EitherT.left[Logger, ErrorType, T](x.pure[Logger])
+    def fromOption[T](x: Option[T], default: => ErrorType): Target[T] = EitherT.fromOption(x, default)
+    def unsafeExtract[T](x: Type[T]): T = x.valueOr({ err => throw new Exception(err.toString) }).value
   }
 
   object CoreTarget {
-    type Type[A] = Either[Error, A]
-    def pure[T](x: T): CoreTarget[T] = Either.right(x)
-    def fromOption[T](x: Option[T], default: => Error): CoreTarget[T] = Either.fromOption(x, default)
-    def error[T](x: Error): CoreTarget[T] = Either.left[Error, T](x)
-    def unsafeExtract[T](x: CoreTarget[T]): T = x.valueOr({ err => throw new Exception(err.toString) })
+    type ErrorType = Error
+    type Type[A] = EitherT[Id, ErrorType, A]
+    val A = Applicative[CoreTarget]
+    def pure[T](x: T): CoreTarget[T] = A.pure(x)
+    def fromOption[T](x: Option[T], default: => ErrorType): CoreTarget[T] = EitherT.fromOption(x, default)
+    def error[T](x: ErrorType): CoreTarget[T] = EitherT.left[Id, ErrorType, T](x)
+    def unsafeExtract[T](x: Type[T]): T = x.valueOr({ err => throw new Exception(err.toString) })
   }
 }
 
