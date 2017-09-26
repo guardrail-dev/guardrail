d180d3595e2d380013353122ac85d2167aab3206
Devon Stewart <dstewart@twilio.com>
2017-09-25 23:57:00 -0700

    Replacing List[String] loggers with StructuredLogger

--- modules/codegen/src/main/scala/com/twilio/swagger/codegen/package.scala
+++ modules/codegen/src/main/scala/com/twilio/swagger/codegen/package.scala
@@ -1,7 +1,7 @@
 package com.twilio.swagger
 
 import cats.{Applicative, Id}
-import cats.data.{Coproduct, EitherT, WriterT}
+import cats.data.{Coproduct, EitherT, NonEmptyList, WriterT}
 import cats.instances.all._
 import cats.syntax.applicative._
 import cats.syntax.either._
@@ -10,29 +10,45 @@ import com.twilio.swagger.codegen.terms.client.ClientTerm
 import com.twilio.swagger.codegen.terms.protocol.{AliasProtocolTerm, EnumProtocolTerm, ModelProtocolTerm, ProtocolSupportTerm}
 import com.twilio.swagger.codegen.terms.server.ServerTerm
 import scala.meta._
+import com.twilio.swagger.core.StructuredLogger
 
 package codegen {
   case class CodegenDefinitions(clients: List[Client], servers: List[Server], frameworkImports: List[Import])
 
   object Target {
     type ErrorType = String
-    type Logger[T] = WriterT[Id, List[String], T]
+    type Logger[T] = WriterT[Id, StructuredLogger, T]
     type Type[A] = EitherT[Logger, ErrorType, A]
     val A = Applicative[Target]
     def pure[T](x: T): Target[T] = A.pure(x)
     def error[T](x: ErrorType): Target[T] = EitherT.left[Logger, ErrorType, T](x.pure[Logger])
     def fromOption[T](x: Option[T], default: => ErrorType): Target[T] = EitherT.fromOption(x, default)
     def unsafeExtract[T](x: Type[T]): T = x.valueOr({ err => throw new Exception(err.toString) }).value
+
+    object log {
+      def debug(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.debug(NonEmptyList(name, names.toList), message)))
+      def info(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.info(NonEmptyList(name, names.toList), message)))
+      def warning(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.warning(NonEmptyList(name, names.toList), message)))
+      def error(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.error(NonEmptyList(name, names.toList), message)))
+    }
   }
 
   object CoreTarget {
     type ErrorType = Error
-    type Type[A] = EitherT[Id, ErrorType, A]
+    type Logger[T] = WriterT[Id, StructuredLogger, T]
+    type Type[A] = EitherT[Logger, ErrorType, A]
     val A = Applicative[CoreTarget]
     def pure[T](x: T): CoreTarget[T] = A.pure(x)
     def fromOption[T](x: Option[T], default: => ErrorType): CoreTarget[T] = EitherT.fromOption(x, default)
-    def error[T](x: ErrorType): CoreTarget[T] = EitherT.left[Id, ErrorType, T](x)
-    def unsafeExtract[T](x: Type[T]): T = x.valueOr({ err => throw new Exception(err.toString) })
+    def error[T](x: ErrorType): CoreTarget[T] = EitherT.left[Logger, ErrorType, T](x.pure[Logger])
+    def unsafeExtract[T](x: Type[T]): T = x.valueOr({ err => throw new Exception(err.toString) }).value
+
+    object log {
+      def debug(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.debug(NonEmptyList(name, names.toList), message)))
+      def info(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.info(NonEmptyList(name, names.toList), message)))
+      def warning(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.warning(NonEmptyList(name, names.toList), message)))
+      def error(name: String, names: String*)(message: String): Type[Unit] = EitherT.right(WriterT.tell(StructuredLogger.error(NonEmptyList(name, names.toList), message)))
+    }
   }
 }
 
