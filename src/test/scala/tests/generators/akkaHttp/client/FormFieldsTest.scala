eb2b6b6ab8a8d627f4f81db5b3bc9283606dab07
Devon Stewart <dstewart@twilio.com>
2018-06-14 17:33:13 -0700

    Adding a test for multipart form field clients

---
+++ src/test/scala/tests/generators/akkaHttp/client/FormFieldsTest.scala
@@ -0,0 +1,79 @@
+package tests.generators.akkaHttp.client
+
+import com.twilio.guardrail.generators.AkkaHttp
+import com.twilio.guardrail.{Client, Clients, Context}
+import org.scalatest.{FunSuite, Matchers}
+import support.SwaggerSpecRunner
+
+import scala.meta._
+
+class FormFieldsTest extends FunSuite with Matchers with SwaggerSpecRunner {
+  val swagger: String = s"""
+    |swagger: "2.0"
+    |info:
+    |  title: Whatever
+    |  version: 1.0.0
+    |host: localhost:1234
+    |schemes:
+    |  - http
+    |paths:
+    |  /foo:
+    |    put:
+    |      operationId: putFoo
+    |      consumes:
+    |        - multipart/form-data
+    |      parameters:
+    |        - name: foo
+    |          in: formData
+    |          type: string
+    |          required: true
+    |        - name: bar
+    |          in: formData
+    |          type: integer
+    |          format: int64
+    |          required: true
+    |        - name: baz
+    |          in: formData
+    |          type: file
+    |          required: true
+    |      responses:
+    |        200:
+    |          description: Success
+    |""".stripMargin
+
+  test("Properly handle all methods") {
+    val (
+      _,
+      Clients(Client(tags, className, statements) :: _),
+      _
+    ) = runSwaggerSpec(swagger)(Context.empty, AkkaHttp)
+
+    val Seq(cmp, cls) = statements.dropWhile(_.isInstanceOf[Import])
+
+    val client = q"""
+      class Client(host: String = "http://localhost:1234")(implicit httpClient: HttpRequest => Future[HttpResponse], ec: ExecutionContext, mat: Materializer) {
+        val basePath: String = ""
+        private[this] def wrap[T: FromEntityUnmarshaller](resp: Future[HttpResponse]): EitherT[Future, Either[Throwable, HttpResponse], T] = {
+          EitherT(resp.flatMap(resp => if (resp.status.isSuccess) {
+            Unmarshal(resp.entity).to[T].map(Right.apply _)
+          } else {
+            FastFuture.successful(Left(Right(resp)))
+          }).recover({
+            case e: Throwable =>
+              Left(Left(e))
+          }))
+        }
+        def putFoo(foo: String, bar: Long, baz: BodyPartEntity, headers: scala.collection.immutable.Seq[HttpHeader] = Nil): EitherT[Future, Either[Throwable, HttpResponse], IgnoredEntity] = {
+          val allHeaders = headers ++ scala.collection.immutable.Seq[Option[HttpHeader]]().flatten
+          wrap[IgnoredEntity](Marshal(Multipart.FormData(Source.fromIterator {
+            () => List(Some(Multipart.FormData.BodyPart("foo", Formatter.show(foo))), Some(Multipart.FormData.BodyPart("bar", Formatter.show(bar))), Some(Multipart.FormData.BodyPart("baz", baz))).flatten.iterator
+          })).to[RequestEntity].flatMap {
+            entity => httpClient(HttpRequest(method = HttpMethods.PUT, uri = host + basePath + "/foo", entity = entity, headers = allHeaders))
+          })
+        }
+      }
+    """
+
+    cls.structure should equal(client.structure)
+  }
+}
---
+++ src/test/scala/tests/generators/akkaHttp/server/FormFieldsTest.scala
@@ -0,0 +1,185 @@
+package tests.generators.akkaHttp.server
+
+import com.twilio.guardrail.generators.AkkaHttp
+import com.twilio.guardrail.{Context, Server, Servers}
+import org.scalatest.{FunSuite, Matchers}
+import support.SwaggerSpecRunner
+
+class FormFieldsServerTest extends FunSuite with Matchers with SwaggerSpecRunner {
+  import scala.meta._
+
+  val swagger: String = s"""
+    |swagger: "2.0"
+    |info:
+    |  title: Whatever
+    |  version: 1.0.0
+    |host: localhost:1234
+    |schemes:
+    |  - http
+    |paths:
+    |  /foo:
+    |    put:
+    |      operationId: putFoo
+    |      consumes:
+    |        - multipart/form-data
+    |      parameters:
+    |        - name: foo
+    |          in: formData
+    |          type: string
+    |          required: true
+    |        - name: bar
+    |          in: formData
+    |          type: integer
+    |          format: int64
+    |          required: true
+    |        - name: baz
+    |          in: formData
+    |          type: file
+    |          required: true
+    |      responses:
+    |        200:
+    |          description: Success
+    |""".stripMargin
+
+  test("Ensure routes are generated") {
+    val (
+      _,
+      _,
+      Servers(Server(pkg, extraImports, genHandler :: genResource :: Nil) :: Nil)
+    ) = runSwaggerSpec(swagger)(Context.empty, AkkaHttp)
+
+    val handler = q"""
+      trait Handler {
+        def putFoo(respond: Resource.putFooResponse.type)(foo: String, bar: Long, baz: (File, Option[String], ContentType, Option[String])): scala.concurrent.Future[Resource.putFooResponse]
+        def putFooMapFileField(fieldName: String, fileName: Option[String], contentType: ContentType): File
+        def putFooUnmarshalToFile(hashType: Option[String], destFn: (String, Option[String], ContentType) => File)(implicit mat: Materializer): Unmarshaller[Multipart.FormData.BodyPart, (File, Option[String], ContentType, Option[String])] = Unmarshaller { implicit executionContext =>
+          part => {
+            val dest = destFn(part.name, part.filename, part.entity.contentType)
+            val messageDigest = hashType.map(MessageDigest.getInstance(_))
+            val fileSink: Sink[ByteString, Future[IOResult]] = FileIO.toPath(dest.toPath).contramap[ByteString] { chunk =>
+              messageDigest.foreach(_.update(chunk.toArray[Byte]))
+              chunk
+            }
+            part.entity.dataBytes.toMat(fileSink)(Keep.right).run().transform({
+              case Failure(t) =>
+                dest.delete()
+                Failure(t)
+              case Success(IOResult(_, Success(_))) =>
+                val hash = messageDigest.map(md => javax.xml.bind.DatatypeConverter.printHexBinary(md.digest()).toLowerCase(java.util.Locale.US))
+                Success((dest, part.filename, part.entity.contentType, hash))
+              case Success(IOResult(_, Failure(t))) =>
+                Failure(t)
+            })
+          }
+        }
+      }
+    """
+    val resource = q"""
+      object Resource {
+        import cats.syntax.either._
+        def discardEntity(implicit mat: akka.stream.Materializer): Directive0 = extractRequest.flatMap { req =>
+          req.discardEntityBytes().future
+          Directive.Empty
+        }
+        implicit def jsonFSU[T: io.circe.Decoder]: Unmarshaller[String, T] = Unmarshaller[String, T] { implicit ev =>
+          string => io.circe.Json.fromString(string).as[T].left.flatMap(err => io.circe.jawn.parse(string).flatMap(_.as[T])).fold(scala.concurrent.Future.failed _, scala.concurrent.Future.successful _)
+        }
+        def routes(handler: Handler)(implicit mat: akka.stream.Materializer): Route = {
+          (put & path("foo") & ({
+            object putFooParts {
+              sealed trait Part
+              case class IgnoredPart(unit: Unit) extends Part
+              case class foo(value: String) extends Part
+              case class bar(value: Long) extends Part
+              case class baz(value: (File, Option[String], ContentType, Option[String])) extends Part
+            }
+            val Unmarshallfoopart: Unmarshaller[Multipart.FormData.BodyPart, putFooParts.foo] = Unmarshaller { implicit executionContext =>
+              part => implicitly[Unmarshaller[Multipart.FormData.BodyPart, String]].apply(part).map(putFooParts.foo.apply)
+            }
+            val Unmarshallbarpart: Unmarshaller[Multipart.FormData.BodyPart, putFooParts.bar] = Unmarshaller { implicit executionContext =>
+              part => implicitly[Unmarshaller[Multipart.FormData.BodyPart, Long]].apply(part).map(putFooParts.bar.apply)
+            }
+            val Unmarshallbazpart: Unmarshaller[Multipart.FormData.BodyPart, putFooParts.baz] = handler.putFooUnmarshalToFile(Option("SHA-512"), handler.putFooMapFileField(_, _, _)).map(putFooParts.baz.apply)
+            val fileReferences = new AtomicReference(List.empty[File])
+            extractExecutionContext.flatMap { implicit executionContext =>
+              extractMaterializer.flatMap { implicit mat =>
+                entity(as[Multipart.FormData]).flatMap { formData =>
+                  val collectedPartsF: Future[Either[Throwable, (Option[String], Option[Long], Option[(File, Option[String], ContentType, Option[String])])]] = for (results <- formData.parts.mapConcat {
+                    part => if (Set[String]("foo", "bar", "baz").contains(part.name)) part :: Nil else {
+                      part.entity.discardBytes()
+                      Nil
+                    }
+                  }.mapAsync(1) { part =>
+                    {
+                      part.name match {
+                        case "foo" =>
+                          SafeUnmarshaller(Unmarshallfoopart).apply(part)
+                        case "bar" =>
+                          SafeUnmarshaller(Unmarshallbarpart).apply(part)
+                        case "baz" =>
+                          SafeUnmarshaller(AccumulatingUnmarshaller(fileReferences, Unmarshallbazpart)(_.value._1)).apply(part)
+                        case _ =>
+                          SafeUnmarshaller(implicitly[Unmarshaller[Multipart.FormData.BodyPart, Unit]].map(putFooParts.IgnoredPart.apply(_))).apply(part)
+                      }
+                    }
+                  }.toMat(Sink.seq[Either[Throwable, putFooParts.Part]])(Keep.right).run()) yield {
+                    results.toList.sequence.map { successes =>
+                      val fooO = successes.collectFirst({
+                        case putFooParts.foo(v1) => v1
+                      })
+                      val barO = successes.collectFirst({
+                        case putFooParts.bar(v1) => v1
+                      })
+                      val bazO = successes.collectFirst({
+                        case putFooParts.baz((v1, v2, v3, v4)) =>
+                          (v1, v2, v3, v4)
+                      })
+                      (fooO, barO, bazO)
+                    }
+                  }
+                  handleExceptions(ExceptionHandler({
+                    case e: Throwable =>
+                      fileReferences.get().foreach(_.delete())
+                      throw e
+                  })) & handleRejections { (rejections: scala.collection.immutable.Seq[Rejection]) =>
+                    fileReferences.get().foreach(_.delete())
+                    None
+                  } & mapResponse { resp =>
+                    fileReferences.get().foreach(_.delete())
+                    resp
+                  } & onSuccess(collectedPartsF)
+                }
+              }
+            }.flatMap(_.fold(t => throw t, {
+              case (fooO, barO, bazO) =>
+                val maybe: Either[Rejection, (String, Long, (File, Option[String], ContentType, Option[String]))] = for (foo <- fooO.toRight(MissingFormFieldRejection("foo")); bar <- barO.toRight(MissingFormFieldRejection("bar")); baz <- bazO.toRight(MissingFormFieldRejection("baz"))) yield {
+                  (foo, bar, baz)
+                }
+                maybe.fold(reject(_), tprovide(_))
+            }))
+          }: Directive[(String, Long, (File, Option[String], ContentType, Option[String]))])) {
+            (foo, bar, baz) => complete(handler.putFoo(putFooResponse)(foo, bar, baz))
+          }
+        }
+        sealed abstract class putFooResponse(val statusCode: StatusCode)
+        case object putFooResponseOK extends putFooResponse(StatusCodes.OK)
+        object putFooResponse {
+          implicit val putFooTRM: ToResponseMarshaller[putFooResponse] = Marshaller { implicit ec =>
+            resp => putFooTR(resp)
+          }
+          implicit def putFooTR(value: putFooResponse)(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[List[Marshalling[HttpResponse]]] = value match {
+            case r: putFooResponseOK.type =>
+              scala.concurrent.Future.successful(Marshalling.Opaque {
+                () => HttpResponse(r.statusCode)
+              } :: Nil)
+          }
+          def apply[T](value: T)(implicit ev: T => putFooResponse): putFooResponse = ev(value)
+          def OK: putFooResponse = putFooResponseOK
+        }
+      }
+    """
+
+    genHandler.structure shouldEqual handler.structure
+    genResource.structure shouldEqual resource.structure
+  }
+}
