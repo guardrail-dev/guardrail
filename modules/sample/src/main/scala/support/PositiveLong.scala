bc7d59063414d8135285f0eba9d0ddb9b9441381
Devon Stewart <dstewart@twilio.com>
2018-07-07 15:14:52 -0700

    Formatting

--- modules/sample/src/main/scala/support/PositiveLong.scala
+++ modules/sample/src/main/scala/support/PositiveLong.scala
@@ -7,7 +7,7 @@ import io.circe.Decoder
 class PositiveLong private (val value: Long) extends AnyVal
 object PositiveLong {
   def apply(value: Long): Option[PositiveLong] = if (value >= 0) Some(new PositiveLong(value)) else None
-  implicit val akkaShowable = AkkaImplicits.Show.build[PositiveLong](_.value.toString())
-  implicit val http4sShowable = Http4sImplicits.Show.build[PositiveLong](_.value.toString())
-  implicit val decodePositiveLong = Decoder.decodeLong.emap(num => PositiveLong.apply(num).toRight(s"${num} is not positive"))
+  implicit val akkaShowable                    = AkkaImplicits.Show.build[PositiveLong](_.value.toString())
+  implicit val http4sShowable                  = Http4sImplicits.Show.build[PositiveLong](_.value.toString())
+  implicit val decodePositiveLong              = Decoder.decodeLong.emap(num => PositiveLong.apply(num).toRight(s"${num} is not positive"))
 }
