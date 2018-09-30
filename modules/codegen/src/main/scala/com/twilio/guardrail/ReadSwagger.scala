c09a38dadf053f40e5021e8342fd553f9b866098
Devon Stewart <blast@hardchee.se>
2018-09-30 10:02:57 -0700

    Factoring out unsafe aspects of ReadSwagger

--- modules/codegen/src/main/scala/com/twilio/guardrail/ReadSwagger.scala
+++ modules/codegen/src/main/scala/com/twilio/guardrail/ReadSwagger.scala
@@ -6,16 +6,24 @@ import cats._
 import java.nio.file.Path
 import scala.io.AnsiColor
 import scala.util.Try
+import cats.syntax.either._
 
 case class ReadSwagger[T](path: Path, next: Swagger => T)
 object ReadSwagger {
+  @deprecated("0.37.1", "Hiding the error result prevents build tools from failing on file read")
   def unsafeReadSwagger[T: Monoid](rs: ReadSwagger[T]): T =
-    (for {
-      absolutePath <- Try(rs.path.toAbsolutePath.toString).toOption
-      swagger      <- Option(new SwaggerParser().read(absolutePath))
-    } yield rs.next(swagger))
-      .getOrElse {
-        println(s"${AnsiColor.RED}Spec file ${rs.path} is either incorrectly formatted or missing.${AnsiColor.RESET}")
+    readSwagger(rs)
+      .fold({ err =>
+        println(s"${AnsiColor.RED}${err}${AnsiColor.RESET}")
         Monoid.empty[T]
-      }
+      }, identity)
+
+  def readSwagger[T](rs: ReadSwagger[T]): Either[String, T] =
+    Either.fromOption(
+      (for {
+        absolutePath <- Try(rs.path.toAbsolutePath.toString).toOption
+        swagger      <- Option(new SwaggerParser().read(absolutePath))
+      } yield rs.next(swagger)),
+      s"Spec file ${rs.path} is either incorrectly formatted or missing."
+    )
 }
