b2713bb31e8395340a4c8aaf75583ac56f65c268
Nicholas Funnell <nfunnell@twilio.com>
2018-10-31 18:38:14 -0700

    server generator should support OPTIONS http method

---
+++ modules/codegen/src/test/scala/core/issues/Issue126.scala
@@ -0,0 +1,69 @@
+package tests.core.issues
+
+import com.twilio.guardrail.generators.AkkaHttp
+import com.twilio.guardrail.{ Context, Server, Servers }
+import org.scalatest.{ FunSuite, Matchers }
+import support.SwaggerSpecRunner
+import com.twilio.guardrail.tests._
+
+class Issue126 extends FunSuite with Matchers with SwaggerSpecRunner {
+  import scala.meta._
+
+  val swagger: String = s"""
+    |swagger: '2.0'
+    |host: petstore.swagger.io
+    |paths:
+    |  /:
+    |    options:
+    |      x-scala-package: store
+    |      operationId: getRoot
+    |      responses:
+    |        200:
+    |          description: Successful
+    |""".stripMargin
+
+  test("Ensure routes are generated for OPTIONS method") {
+    val (
+      _,
+      _,
+      Servers(Server(pkg, extraImports, genHandler :: genResource :: Nil) :: Nil)
+    ) = runSwaggerSpec(swagger)(Context.empty, AkkaHttp, defaults.akkaGeneratorSettings)
+
+    val handler  = q"""
+      trait StoreHandler {
+        def getRoot(respond: StoreResource.getRootResponse.type)(): scala.concurrent.Future[StoreResource.getRootResponse]
+      }
+    """
+    val resource = q"""
+      object StoreResource {
+        def discardEntity(implicit mat: akka.stream.Materializer): Directive0 = extractRequest.flatMap { req =>
+          req.discardEntityBytes().future
+          Directive.Empty
+        }
+        def routes(handler: StoreHandler)(implicit mat: akka.stream.Materializer): Route = {
+          (options & pathEndOrSingleSlash & discardEntity) {
+            complete(handler.getRoot(getRootResponse)())
+          }
+        }
+        sealed abstract class getRootResponse(val statusCode: StatusCode)
+        case object getRootResponseOK extends getRootResponse(StatusCodes.OK)
+        object getRootResponse {
+          implicit val getRootTRM: ToResponseMarshaller[getRootResponse] = Marshaller { implicit ec =>
+            resp => getRootTR(resp)
+          }
+          implicit def getRootTR(value: getRootResponse)(implicit ec: scala.concurrent.ExecutionContext): scala.concurrent.Future[List[Marshalling[HttpResponse]]] = value match {
+            case r: getRootResponseOK.type =>
+              scala.concurrent.Future.successful(Marshalling.Opaque {
+                () => HttpResponse(r.statusCode)
+              } :: Nil)
+          }
+          def apply[T](value: T)(implicit ev: T => getRootResponse): getRootResponse = ev(value)
+          def OK: getRootResponse = getRootResponseOK
+        }
+      }
+    """
+
+    genHandler.structure shouldEqual handler.structure
+    genResource.structure shouldEqual resource.structure
+  }
+}
