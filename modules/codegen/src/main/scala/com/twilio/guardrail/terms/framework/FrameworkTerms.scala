2dce6a8eec356afdcf0d9eff770603555e8d9cf0
Devon Stewart <blast@hardchee.se>
2018-12-01 13:41:08 -0800

    Moving lookupStatusCode into FrameworkTerms

--- modules/codegen/src/main/scala/com/twilio/guardrail/terms/framework/FrameworkTerms.scala
+++ modules/codegen/src/main/scala/com/twilio/guardrail/terms/framework/FrameworkTerms.scala
@@ -13,6 +13,8 @@ class FrameworkTerms[L <: LA, F[_]](implicit I: InjectK[FrameworkTerm[L, ?], F])
     Free.inject[FrameworkTerm[L, ?], F](GetFrameworkImplicits[L]())
   def getGeneratorSettings(): Free[F, GeneratorSettings[L]] =
     Free.inject[FrameworkTerm[L, ?], F](GetGeneratorSettings[L]())
+  def lookupStatusCode(key: String): Free[F, (Int, L#TermName)] =
+    Free.inject[FrameworkTerm[L, ?], F](LookupStatusCode(key))
 }
 
 object FrameworkTerms {
