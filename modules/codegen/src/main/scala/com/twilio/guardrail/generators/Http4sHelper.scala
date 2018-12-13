35214b776e125bddbcda9812294ea2cea23c509d
Devon Stewart <blast@hardchee.se>
2018-12-13 02:05:06 -0800

    Removing old getResponses

--- modules/codegen/src/main/scala/com/twilio/guardrail/generators/Http4sHelper.scala
+++ modules/codegen/src/main/scala/com/twilio/guardrail/generators/Http4sHelper.scala
@@ -49,13 +49,6 @@ object Http4sHelper {
     } yield new Responses[L](instances)
   }
 
-  def getResponses(operationId: String, operation: Operation, protocolElems: List[StrictProtocolElems[ScalaLanguage]]): Target[Responses[ScalaLanguage]] = {
-    type Program[T] = EitherK[ScalaTerm[ScalaLanguage, ?], EitherK[SwaggerTerm[ScalaLanguage, ?], FrameworkTerm[ScalaLanguage, ?], ?], T]
-    val interp = ScalaGenerator.ScalaInterp.or(SwaggerGenerator.SwaggerInterp.or(Http4sGenerator.FrameworkInterp))
-    getResponses[ScalaLanguage, Program](operationId, operation, protocolElems)
-      .foldMap(interp)
-  }
-
   def generateResponseDefinitions(operationId: String,
                                   responses: Responses[ScalaLanguage],
                                   protocolElems: List[StrictProtocolElems[ScalaLanguage]]): List[Defn] = {
