a89d33d5ab56624d346788c424dd8ee4041f509e
Devon Stewart <blast@hardchee.se>
2018-12-15 11:23:03 -0800

    Removing GeneratorSettings import

--- modules/codegen/src/main/scala/com/twilio/guardrail/protocol/terms/protocol/EnumProtocolTerm.scala
+++ modules/codegen/src/main/scala/com/twilio/guardrail/protocol/terms/protocol/EnumProtocolTerm.scala
@@ -1,7 +1,6 @@
 package com.twilio.guardrail.protocol.terms.protocol
 
 import _root_.io.swagger.models.ModelImpl
-import com.twilio.guardrail.generators.GeneratorSettings
 import com.twilio.guardrail.languages.LA
 
 sealed trait EnumProtocolTerm[L <: LA, T]
