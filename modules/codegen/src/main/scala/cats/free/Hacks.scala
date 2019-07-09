d992f79e00227b3be3aadbd8414444caa4758ca9
Devon Stewart <blast@hardchee.se>
2019-07-08 21:45:51 -0700

    Traversing Free structure to inject term logging

---
+++ modules/codegen/src/main/scala/cats/free/Hacks.scala
@@ -0,0 +1,60 @@
+package cats.free
+
+import cats.data.EitherK
+import cats.free.Free.{ FlatMapped, Pure, Suspend }
+import cats.implicits._
+
+object GuardrailFreeHacks {
+  val limit: Int = 80
+
+  trait TermName[F[_]] { def extract[A](term: F[A]): (String, String) }
+
+  trait LowPriorityTermName {
+    implicit def baseTermName[F[_]]: TermName[F] = new TermName[F] {
+      def extract[A](term: F[A]) =
+        (
+          term.getClass.getSimpleName,
+          term
+            .toString()
+            .lines
+            .filterNot(_.contains(": null"))
+            .mkString("; ")
+        )
+    }
+  }
+
+  trait HighPriorityTermName extends LowPriorityTermName {
+    implicit def extractEitherT[F[_], G[_]](implicit evF: TermName[F], evG: TermName[G]): TermName[EitherK[F, G, ?]] = new TermName[EitherK[F, G, ?]] {
+      def extract[A](term: EitherK[F, G, A]) = term.run.fold(evF.extract _, evG.extract _)
+    }
+  }
+
+  object TermName extends HighPriorityTermName {
+    def apply[F[_], A](value: F[A])(implicit ev: TermName[F]): String = {
+      val (name, full) = ev.extract(value)
+      if (full.length > limit) name else full
+    }
+    def full[F[_], A](value: F[A])(implicit ev: TermName[F]): String     = ev.extract(value)._2
+    def nameOnly[F[_], A](value: F[A])(implicit ev: TermName[F]): String = ev.extract(value)._1
+  }
+
+  def injectLogs[F[_]: TermName, A](value: Free[F, A],
+                                    ignore: Set[String],
+                                    push: String => Free[F, Unit],
+                                    pop: Free[F, Unit],
+                                    emit: Free[F, Unit]): Free[F, A] = {
+    def next[B]: Free[F, B] => Free[F, B] = injectLogs(_, ignore, push, pop, emit)
+    value match {
+      case Pure(a) =>
+        Pure(a)
+      case Suspend(a) =>
+        Suspend(a)
+      case term @ FlatMapped(Suspend(a), _) if ignore contains TermName.nameOnly(a) =>
+        term
+      case FlatMapped(Suspend(a), f) =>
+        push(TermName(a)) *> FlatMapped(Suspend(a), f.map(x => next(x) <* emit <* pop))
+      case FlatMapped(c, f) =>
+        FlatMapped(next(c), f.map(next))
+    }
+  }
+}
