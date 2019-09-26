9e493f5625a43f41671a3c86504b9c78e0ffa1a0
Evan Fossier <efossier@twilio.com>
2019-09-26 17:20:54 +0200

    Fix Issue405 tests

--- modules/sample-akkaHttp/src/test/scala/core/issues/Issue405.scala
+++ modules/sample-akkaHttp/src/test/scala/core/issues/Issue405.scala
@@ -1,11 +1,12 @@
 package core.issues
 
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.RejectionHandler
+import akka.http.scaladsl.server.{RejectionHandler, Route}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.SpanSugar._
 import org.scalatest.{EitherValues, FunSuite, Matchers}
+import akka.http.scaladsl.model.FormData
 import akka.http.scaladsl.server.Directives.complete
 
 import scala.concurrent.Future
@@ -18,33 +19,23 @@ import scala.concurrent.Future
 class Issue405 extends FunSuite with Matchers with EitherValues with ScalaFutures with ScalatestRouteTest {
   override implicit val patienceConfig = PatienceConfig(10 seconds, 1 second)
 
+  implicit val rejectionHandler: RejectionHandler = RejectionHandler.newBuilder().handle { case rej =>
+    complete(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, s"Request failed with $rej")))
+  }.result()
+
   test("Empty string for a required form param should parse as empty string") {
     import issues.issue405.server.akkaHttp.{ Handler, Resource }
     import issues.issue405.server.akkaHttp.definitions._
 
-    implicit def rejectionHandler =
-      RejectionHandler.newBuilder()
-        .handle { case rej =>
-          complete(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, s"Request failed with $rej")))
-        }
-        .result()
-
-    val route = Resource.routes(new Handler {
+    val route = Route.seal(Resource.routes(new Handler {
       override def foo(respond: Resource.fooResponse.type)(bar: String, baz: Option[String]): Future[Resource.fooResponse] =
         Future.successful(respond.OK(s"Bar is '$bar'"))
-    })
+    }))
 
     /* Pass empty string to required Bar param */
-    Post("/Foo")
-      .withEntity(
-        Multipart
-          .FormData(
-            Multipart.FormData.BodyPart.Strict("Bar", "")
-          )
-          .toEntity
-      ) ~> route ~> check {
+    Post("/v1/Foo", FormData(Map("Bar" -> ""))) ~> route ~> check {
       response.status shouldBe (StatusCodes.OK)
-      response.entity.asInstanceOf[String] shouldBe "Bar is ''"
+      responseAs[String] shouldBe "\"Bar is ''\""
     }
   }
 
@@ -52,24 +43,17 @@ class Issue405 extends FunSuite with Matchers with EitherValues with ScalaFuture
     import issues.issue405.server.akkaHttp.{ Handler, Resource }
     import issues.issue405.server.akkaHttp.definitions._
 
-    val route = Resource.routes(new Handler {
+    val route = Route.seal(Resource.routes(new Handler {
       override def foo(respond: Resource.fooResponse.type)(bar: String, baz: Option[String]): Future[Resource.fooResponse] = {
         val msg = baz.map(s => s"present: '$s'").getOrElse("missing")
         Future.successful(respond.OK(s"Baz is $msg"))
       }
-    })
+    }))
 
     /* Pass empty string to required Bar param */
-    Post("/Foo")
-      .withEntity(
-        Multipart
-          .FormData(
-            Multipart.FormData.BodyPart.Strict("Baz", "")
-          )
-          .toEntity
-      ) ~> route ~> check {
+    Post("/v1/Foo", FormData(Map("Bar" -> "whatevs", "Baz" -> ""))) ~> route ~> check {
       response.status shouldBe (StatusCodes.OK)
-      response.entity.asInstanceOf[String] shouldBe "Baz is present: ''"
+      responseAs[String] shouldBe "\"Baz is present: ''\""
     }
   }
 }
