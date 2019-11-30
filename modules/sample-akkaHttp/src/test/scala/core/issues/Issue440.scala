2223e2ebe63b2061f1c2cab2ddd8030338fe74d9
Devon Stewart <blast@hardchee.se>
2019-11-29 22:07:46 -0800

    Adding tests for the customized map

--- modules/sample-akkaHttp/src/test/scala/core/issues/Issue440.scala
+++ modules/sample-akkaHttp/src/test/scala/core/issues/Issue440.scala
@@ -5,8 +5,8 @@ import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.unmarshalling.Unmarshaller
-import cats.data.NonEmptyList
-import cats.instances.future._
+import cats.data.{ NonEmptyList, NonEmptyMap }
+import cats.implicits._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.SpanSugar._
 import org.scalatest.{ EitherValues, FunSuite, Matchers }
@@ -15,7 +15,7 @@ import io.circe._
 
 class Issue440Suite extends FunSuite with Matchers {
   test("Construct Issue 440 elements") {
-    import issues.issue440.server.akkaHttp.definitions.{ Bar, Foo }
-    Foo(NonEmptyList.of(Bar(1L)), NonEmptyList.of(Bar(2L)))
+    import issues.issue440.server.akkaHttp.definitions.{ Bar, Enum, Foo }
+    Foo(NonEmptyList.of(Bar(1L)), NonEmptyList.of(Bar(2L)), NonEmptyMap.of(("a", Enum.B), ("b", Enum.C)))
   }
 }
