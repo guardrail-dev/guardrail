32adfcb549a6c2f719a28d529c81bec009648c77
Devon Stewart <blast@hardchee.se>
2020-02-26 00:50:35 -0800

    Reordering tag prompts

--- support/write-release-template.sh
+++ support/write-release-template.sh
@@ -1,5 +1,12 @@
 #!/bin/env bash
 
+last_tag="$(git tag | tail -n1)"
+read -p "Last release (${last_tag}): " new_last_tag
+if [ ! -z "${new_last_tag}" ]; then
+        last_tag="${new_last_tag}"
+        unset new_last_tag
+fi
+
 new_tag="$1"
 
 if [ -z "${new_tag}" ]; then
@@ -9,13 +16,6 @@ if [ -z "${new_tag}" ]; then
         fi
 fi
 notes_file="notes/${new_tag#v}.md"
-
-last_tag="$(git tag | tail -n1)"
-read -p "Last release (${last_tag}): " new_last_tag
-if [ ! -z "${new_last_tag}" ]; then
-        last_tag="${new_last_tag}"
-        unset new_last_tag
-fi
 
 if [ -e "$notes_file" ]; then
         echo "Error: ${notes_file} already exists"
