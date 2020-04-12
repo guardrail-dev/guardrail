ccee59249e1d60b4f1a9e6a19fcec20eb7d23048
Devon Stewart <blast@hardchee.se>
2020-04-11 19:26:51 -0700

    Hackily adapting SpringMvcClientInterp

--- modules/codegen/src/main/scala/com/twilio/guardrail/generators/Java/SpringMvcClientGenerator.scala
+++ modules/codegen/src/main/scala/com/twilio/guardrail/generators/Java/SpringMvcClientGenerator.scala
@@ -1,37 +1,69 @@
 package com.twilio.guardrail.generators.Java
 
-import cats.arrow.FunctionK
 import com.twilio.guardrail.Target
 import com.twilio.guardrail.languages.JavaLanguage
 import com.twilio.guardrail.protocol.terms.client._
+import cats.data.NonEmptyList
+import com.twilio.guardrail.generators.ScalaParameters
+import com.twilio.guardrail.protocol.terms.Responses
+import com.twilio.guardrail.terms.{ RouteMeta, SecurityScheme }
+import com.twilio.guardrail.StrictProtocolElems
+import java.net.URI
 
 object SpringMvcClientGenerator {
 
-  object ClientTermInterp extends FunctionK[ClientTerm[JavaLanguage, ?], Target] {
-    def apply[T](term: ClientTerm[JavaLanguage, T]): Target[T] = term match {
-      case GenerateClientOperation(className, route, methodName, tracing, parameters, responses, securitySchemes) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case GetImports(tracing) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case GetExtraImports(tracing) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case ClientClsArgs(tracingName, serverUrls, tracing) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case GenerateResponseDefinitions(operationId, responses, protocolElems) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case GenerateSupportDefinitions(tracing, securitySchemes) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case BuildStaticDefns(clientName, tracingName, serverUrls, ctorArgs, tracing) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-
-      case BuildClient(clientName, tracingName, serverUrls, basePath, ctorArgs, clientCalls, supportDefinitions, tracing) =>
-        Target.raiseUserError("spring client generation is not currently supported")
-    }
+  object ClientTermInterp extends ClientTerms[JavaLanguage, Target] {
+    def MonadF = Target.targetInstances
+    def generateClientOperation(
+        className: List[String],
+        tracing: Boolean,
+        securitySchemes: Map[String, SecurityScheme[JavaLanguage]],
+        parameters: ScalaParameters[JavaLanguage]
+    )(
+        route: RouteMeta,
+        methodName: String,
+        responses: Responses[JavaLanguage]
+    ) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def getImports(tracing: Boolean) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def getExtraImports(tracing: Boolean) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def clientClsArgs(
+        tracingName: Option[String],
+        serverUrls: Option[NonEmptyList[URI]],
+        tracing: Boolean
+    ) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def generateResponseDefinitions(
+        operationId: String,
+        responses: Responses[JavaLanguage],
+        protocolElems: List[StrictProtocolElems[JavaLanguage]]
+    ) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def generateSupportDefinitions(
+        tracing: Boolean,
+        securitySchemes: Map[String, SecurityScheme[JavaLanguage]]
+    ) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def buildStaticDefns(
+        clientName: String,
+        tracingName: Option[String],
+        serverUrls: Option[NonEmptyList[URI]],
+        ctorArgs: List[List[com.github.javaparser.ast.body.Parameter]],
+        tracing: Boolean
+    ) =
+      Target.raiseUserError("spring client generation is not currently supported")
+    def buildClient(
+        clientName: String,
+        tracingName: Option[String],
+        serverUrls: Option[NonEmptyList[URI]],
+        basePath: Option[String],
+        ctorArgs: List[List[com.github.javaparser.ast.body.Parameter]],
+        clientCalls: List[com.github.javaparser.ast.body.BodyDeclaration[_ <: com.github.javaparser.ast.body.BodyDeclaration[_]]],
+        supportDefinitions: List[com.github.javaparser.ast.body.BodyDeclaration[_ <: com.github.javaparser.ast.body.BodyDeclaration[_]]],
+        tracing: Boolean
+    ) =
+      Target.raiseUserError("spring client generation is not currently supported")
   }
 }
