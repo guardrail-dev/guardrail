3525dd3c22880607f48ec475002613126d776b05
Devon Stewart <blast@hardchee.se>
2020-04-28 11:49:07 -0700

    Adding unapply for Tracker

--- modules/codegen/src/main/scala/com/twilio/guardrail/core/Tracker.scala
+++ modules/codegen/src/main/scala/com/twilio/guardrail/core/Tracker.scala
@@ -152,4 +152,5 @@ object Tracker extends HighPriorityTrackerEvidence with HighPriorityTrackerSynta
 
   def apply(swagger: OpenAPI): Tracker[OpenAPI]                     = new Tracker(swagger, Vector.empty)
   def cloneHistory[A, B](tracker: Tracker[A], value: B): Tracker[B] = new Tracker(value, tracker.history)
+  def unapply[A](instance: Tracker[A]): Some[(String, A)]           = Some((instance.showHistory, instance.unwrapTracker))
 }
