af1386054de7b509860560e54b64c10179a63e41
Brian J. Tarricone <brian@tarricone.org>
2020-09-01 20:58:42 -0700

    Actually fix DW resource optional collection param unwrapping
    Looks like I messed it up when I implemented this fix before; added a
    test this time to make sure it works.
    
    The background is that Jersey can't support nested collections types as
    resource method parameters.  So Optional<List<T>> does not work.
    Instead, we want to just unwrap that as List<T>, and Jersey will give us
    an empty list if it's not provided.

---
+++ modules/codegen/src/test/scala/tests/generators/dropwizard/server/DropwizardOptionalArrayParamTest.scala
@@ -0,0 +1,75 @@
+package tests.generators.dropwizard.server
+
+import com.github.javaparser.ast.body.MethodDeclaration
+import com.twilio.guardrail.{ Context, Server, Servers }
+import com.twilio.guardrail.generators.Java.Dropwizard
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import scala.collection.JavaConverters._
+import scala.compat.java8.OptionConverters._
+import support.SwaggerSpecRunner
+
+class DropwizardOptionalArrayParamTest extends AnyFreeSpec with Matchers with SwaggerSpecRunner {
+  private val openapi =
+    """openapi: 3.0.2
+      |info:
+      |  title: Foo
+      |  version: 1.0.0
+      |paths:
+      |  /foo:
+      |    get:
+      |      operationId: getFoo
+      |      parameters:
+      |        - name: optstuff
+      |          in: query
+      |          required: false
+      |          schema:
+      |            type: array
+      |            items:
+      |              type: string
+      |        - name: reqstuff
+      |          in: query
+      |          required: true
+      |          schema:
+      |            type: array
+      |            items:
+      |              type: string
+      |      responses:
+      |        200: {}
+      |""".stripMargin
+
+  "Optional array resource method params should be unwrapped" in {
+    val (
+      _,
+      _,
+      Servers(Server(_, _, _, genResource :: Nil) :: Nil, _)
+    ) = runSwaggerSpec(openapi)(Context.empty, Dropwizard)
+
+    val getFooMethod = genResource
+      .asClassOrInterfaceDeclaration()
+      .getMembers
+      .asScala
+      .collectFirst({
+        case method: MethodDeclaration if method.getNameAsString == "getFoo" => method
+      })
+      .value
+
+    getFooMethod
+      .getParameterByName("optstuff")
+      .asScala
+      .value
+      .getType
+      .asClassOrInterfaceType
+      .getName
+      .getIdentifier mustBe "List"
+
+    getFooMethod
+      .getParameterByName("reqstuff")
+      .asScala
+      .value
+      .getType
+      .asClassOrInterfaceType
+      .getName
+      .getIdentifier mustBe "List"
+  }
+}
