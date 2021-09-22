42b0c2e18b605a7fe2951d5e1a0c52b0b29a3b19
Devon Stewart <blast@hardchee.se>
2021-09-22 11:55:56 -0700

    Add support for sbtn, if available

--- cli.sh
+++ cli.sh
@@ -9,6 +9,9 @@ args="$@"
 if hash sbt-client 2>&1 >/dev/null; then
     # Use https://github.com/cb372/sbt-client/ if available
     sbt-client "runMain dev.guardrail.CLI ${args}"
+elif hash sbtn 2>&1 >/dev/null; then
+    # Use sbtn if available
+    sbtn "runMain dev.guardrail.CLI ${args}"
 else
     sbt "cli ${args}"
 fi
