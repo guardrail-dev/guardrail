1327ee7d1b18b2589242a186ca707033b6d0456c
Devon Stewart <blast@hardchee.se>
2021-10-14 07:26:36 -0700

    Procol -> Protocol

--- modules/core/src/main/scala/dev/guardrail/terms/protocol/ProtocolSupportTerms.scala
+++ modules/core/src/main/scala/dev/guardrail/terms/protocol/ProtocolSupportTerms.scala
@@ -19,7 +19,7 @@ abstract class ProtocolSupportTerms[L <: LA, F[_]](implicit Cl: CollectionsLibTe
   def copy(
       newMonadF: Monad[F] = this.MonadF,
       newExtractConcreteTypes: Either[String, List[PropMeta[L]]] => F[List[PropMeta[L]]] = this.extractConcreteTypes,
-      newStaticProcolImports: ((List[String]) => F[List[L#Import]]) = this.staticProtocolImports _,
+      newStaticProtocolImports: ((List[String]) => F[List[L#Import]]) = this.staticProtocolImports _,
       newProtocolImports: (() => F[List[L#Import]]) = this.protocolImports _,
       newPackageObjectImports: (() => F[List[L#Import]]) = this.packageObjectImports _,
       newPackageObjectContents: (() => F[List[L#Statement]]) = this.packageObjectContents _,
@@ -28,7 +28,7 @@ abstract class ProtocolSupportTerms[L <: LA, F[_]](implicit Cl: CollectionsLibTe
   ): ProtocolSupportTerms[L, F] = new ProtocolSupportTerms[L, F] {
     def MonadF                                                          = newMonadF
     def extractConcreteTypes(models: Either[String, List[PropMeta[L]]]) = newExtractConcreteTypes(models)
-    def staticProtocolImports(pkgName: List[String])                    = newStaticProcolImports(pkgName)
+    def staticProtocolImports(pkgName: List[String])                    = newStaticProtocolImports(pkgName)
     def protocolImports()                                               = newProtocolImports()
     def packageObjectImports()                                          = newPackageObjectImports()
     def packageObjectContents()                                         = newPackageObjectContents()
