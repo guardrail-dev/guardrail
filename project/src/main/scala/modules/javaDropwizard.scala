f1029b1cefde14ca1eb97e73e0bd10a6a0c9454f
Scala Steward <me@scala-steward.org>
2021-10-14 02:25:19 +0200

    Update scala-java8-compat to 1.0.1

--- project/src/main/scala/modules/javaDropwizard.scala
+++ project/src/main/scala/modules/javaDropwizard.scala
@@ -29,7 +29,7 @@ object javaDropwizard {
     "org.glassfish.jersey.test-framework.providers" % "jersey-test-framework-provider-grizzly2" % jerseyVersion % Test
   ) ++ Seq(
     "org.mockito"                %% "mockito-scala"          % "1.16.46"           % Test,
-    "org.scala-lang.modules"     %% "scala-java8-compat"     % "1.0.0"            % Test,
+    "org.scala-lang.modules"     %% "scala-java8-compat"     % "1.0.1"            % Test,
     "org.scalatest"              %% "scalatest"              % scalatestVersion   % Test,
   ).map(_.cross(CrossVersion.for3Use2_13))
 
