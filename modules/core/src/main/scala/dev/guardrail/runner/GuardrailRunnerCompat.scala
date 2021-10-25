f3b622eb144270ad2af11289bdc5950dadc08c16
Devon Stewart <blast@hardchee.se>
2021-10-25 08:32:14 -0700

    Adding new GuardrailRunnerCompat
    This should permit non-scala build tooling to construct a guardrail runner

---
+++ modules/core/src/main/scala/dev/guardrail/runner/GuardrailRunnerCompat.scala
@@ -0,0 +1,7 @@
+package dev.guardrail.runner
+
+import cats.data.NonEmptyList
+
+import dev.guardrail.{ Args, ReadSwagger, Target, WriteTree }
+
+abstract class GuardrailRunnerCompat(val languages: Map[String, NonEmptyList[Args] => Target[NonEmptyList[ReadSwagger[Target[List[WriteTree]]]]]]) extends GuardrailRunner
