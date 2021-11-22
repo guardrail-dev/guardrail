8e7c0cfab73d944acbc5ecab6c72371a31991857
Devon Stewart <blast@hardchee.se>
2021-11-22 08:45:35 -0800

    Adding bincompat testing for CI

---
+++ support/current-pr-labels.sh
@@ -0,0 +1,10 @@
+if [ -n "$GITHUB_EVENT_PATH" ]; then
+  pr_number="$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")"
+  curl \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    "https://api.github.com/repos/guardrail-dev/guardrail/pulls/${pr_number}" | \
+    jq -r '.labels | map(.name)[]'
+else
+  echo 'Skipping finding labels because GITHUB_EVENT_PATH is not defined' >&2
+fi
