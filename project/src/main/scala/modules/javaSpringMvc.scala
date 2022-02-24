443508e77f33eab3a129a7bde2c78f5b2a993297
Scala Steward <me@scala-steward.org>
2022-02-24 16:26:00 +0100

    Update spring-boot-starter-test, ... to 2.6.4

--- project/src/main/scala/modules/javaSpringMvc.scala
+++ project/src/main/scala/modules/javaSpringMvc.scala
@@ -8,7 +8,7 @@ import sbt.Keys._
 object javaSpringMvc {
   val javaxAnnotationVersion = "1.3.2"
   val scalatestVersion       = "3.2.11"
-  val springBootVersion      = "2.6.3"
+  val springBootVersion      = "2.6.4"
 
   val dependencies = Seq(
     "org.springframework.boot"   %  "spring-boot-starter-web"  % springBootVersion,
