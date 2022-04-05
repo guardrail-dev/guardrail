9089ebdc4136de46cf35709ab7c212de965eeb4c
Devon Stewart <blast@hardchee.se>
2022-04-05 10:50:05 -0700

    Removing class indirection stuff

--- modules/codegen/src/main/scala/dev/guardrail/generators/GeneratorMappings.scala
+++ modules/codegen/src/main/scala/dev/guardrail/generators/GeneratorMappings.scala
@@ -13,30 +13,12 @@ import dev.guardrail.generators.scala.ScalaLanguage
 import dev.guardrail.{ Args, Common, MissingDependency, ReadSwagger, Target, UnparseableArgument, WriteTree }
 
 object GeneratorMappings {
-  private abstract class LoaderIndirection[A] {
-    val instance: A
-  }
-
-  private def catchClassNotFound[A](value: => LoaderIndirection[A], error: => MissingDependency): Target[A] =
-    try
-      Target.pure(value.instance)
-    catch {
-      case _: _root_.java.lang.NoClassDefFoundError =>
-        Target.raiseError(error)
-    }
-
   implicit def scalaInterpreter = new CoreTermInterp[ScalaLanguage](
     "akka-http",
     xs => FrameworkLoader.load[ScalaLanguage](xs, MissingDependency(xs.mkString(", "))),
     { case frameworkName => ModuleMapperLoader.load[ScalaLanguage](frameworkName, MissingDependency(frameworkName)) },
     _.parse[Importer].toEither.bimap(err => UnparseableArgument("import", err.toString), importer => Import(List(importer)))
   )
-
-  private def indirectJavaModule = new LoaderIndirection[AbstractModule[JavaLanguage]] {
-    val instance = java.JavaModule
-  }
-
-  val javaModule = catchClassNotFound(indirectJavaModule, MissingDependency("guardrail-java-support"))
 
   implicit def javaInterpreter = new CoreTermInterp[JavaLanguage](
     "dropwizard",
