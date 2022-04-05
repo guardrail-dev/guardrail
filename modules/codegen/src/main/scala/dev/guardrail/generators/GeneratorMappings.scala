51c495060db5c384ce3e548fa40f0c65c179b24e
Devon Stewart <blast@hardchee.se>
2022-04-05 10:50:40 -0700

    Splitting out Scala/Java generator mappings

--- modules/codegen/src/main/scala/dev/guardrail/generators/GeneratorMappings.scala
+++ modules/codegen/src/main/scala/dev/guardrail/generators/GeneratorMappings.scala
@@ -1,37 +1,14 @@
 package dev.guardrail.generators
 
 import cats.data.NonEmptyList
-import cats.syntax.all._
-import com.github.javaparser.StaticJavaParser
-import _root_.scala.meta._
-import _root_.scala.util.{ Failure, Success, Try }
 
-import dev.guardrail.core.CoreTermInterp
-import dev.guardrail.generators.spi.{ FrameworkLoader, ModuleMapperLoader }
+import dev.guardrail.generators.java.JavaGeneratorMappings.javaInterpreter
 import dev.guardrail.generators.java.JavaLanguage
+import dev.guardrail.generators.scala.ScalaGeneratorMappings.scalaInterpreter
 import dev.guardrail.generators.scala.ScalaLanguage
-import dev.guardrail.{ Args, Common, MissingDependency, ReadSwagger, Target, UnparseableArgument, WriteTree }
+import dev.guardrail.{ Args, Common, ReadSwagger, Target, WriteTree }
 
 object GeneratorMappings {
-  implicit def scalaInterpreter = new CoreTermInterp[ScalaLanguage](
-    "akka-http",
-    xs => FrameworkLoader.load[ScalaLanguage](xs, MissingDependency(xs.mkString(", "))),
-    frameworkName => ModuleMapperLoader.load[ScalaLanguage](frameworkName, MissingDependency(frameworkName)),
-    _.parse[Importer].toEither.bimap(err => UnparseableArgument("import", err.toString), importer => Import(List(importer)))
-  )
-
-  implicit def javaInterpreter = new CoreTermInterp[JavaLanguage](
-    "dropwizard",
-    xs => FrameworkLoader.load[JavaLanguage](xs, MissingDependency(xs.mkString(", "))),
-    frameworkName => ModuleMapperLoader.load[JavaLanguage](frameworkName, MissingDependency(frameworkName)),
-    { str =>
-      Try(StaticJavaParser.parseImport(s"import ${str};")) match {
-        case Success(value) => Right(value)
-        case Failure(t)     => Left(UnparseableArgument("import", t.getMessage))
-      }
-    }
-  )
-
   def defaultLanguages: Map[String, NonEmptyList[Args] => Target[NonEmptyList[ReadSwagger[Target[List[WriteTree]]]]]] = Map(
     ("java", Common.runM[JavaLanguage, Target](_)),
     ("scala", Common.runM[ScalaLanguage, Target](_))
---
+++ modules/scala-support/src/main/scala/dev/guardrail/generators/scala/ScalaGeneratorMappings.scala
@@ -0,0 +1,17 @@
+package dev.guardrail.generators.scala
+
+import cats.syntax.all._
+import _root_.scala.meta._
+
+import dev.guardrail.core.CoreTermInterp
+import dev.guardrail.generators.spi.{ FrameworkLoader, ModuleMapperLoader }
+import dev.guardrail.{ MissingDependency, UnparseableArgument }
+
+object ScalaGeneratorMappings {
+  implicit def scalaInterpreter = new CoreTermInterp[ScalaLanguage](
+    "akka-http",
+    xs => FrameworkLoader.load[ScalaLanguage](xs, MissingDependency(xs.mkString(", "))),
+    frameworkName => ModuleMapperLoader.load[ScalaLanguage](frameworkName, MissingDependency(frameworkName)),
+    _.parse[Importer].toEither.bimap(err => UnparseableArgument("import", err.toString), importer => Import(List(importer)))
+  )
+}
