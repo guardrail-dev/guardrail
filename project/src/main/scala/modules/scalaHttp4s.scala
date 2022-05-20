40d2b56f86bbb1077b6216eb6b070df66d345c4f
Scala Steward <me@scala-steward.org>
2022-05-20 06:41:29 +0200

    Update circe-core, circe-jawn, circe-parser to 0.14.2

--- project/src/main/scala/modules/scalaHttp4s.scala
+++ project/src/main/scala/modules/scalaHttp4s.scala
@@ -7,7 +7,7 @@ import sbt.Keys._
 
 object scalaHttp4s {
   val catsVersion            = "2.7.0"
-  val circeVersion           = "0.14.1"
+  val circeVersion           = "0.14.2"
   val javaxAnnotationVersion = "1.3.2"
   val jaxbApiVersion         = "2.3.1"
   val scalatestVersion       = "3.2.12"
