d2f213c4d5553b9f9a73f131d1fbb3542e5506de
Scala Steward <me@scala-steward.org>
2022-05-25 06:50:09 +0200

    Update http4s-blaze-client, ... to 0.23.12

--- project/src/main/scala/modules/scalaHttp4s.scala
+++ project/src/main/scala/modules/scalaHttp4s.scala
@@ -34,7 +34,7 @@ object scalaHttp4s {
 
   val dependencies = {
     val catsEffectVersion      = "3.3.12"
-    val http4sVersion          = "0.23.11"
+    val http4sVersion          = "0.23.12"
 
     Seq(
       "javax.annotation" %  "javax.annotation-api"  % javaxAnnotationVersion, // for jdk11
