8ddef908bfc10e09b203c001f45479e96fed9bc6
Devon Stewart <blast@hardchee.se>
2022-06-21 22:07:44 -0700

    Finally figured out the data flow issues
    1) Monad was not what I wanted, I needed Applicative
    2) "missing" needed to be a completely new identifier, not just whatever modules we have attempted so far

--- modules/core/src/main/scala/dev/guardrail/generators/spi/ModuleLoadResult.scala
+++ modules/core/src/main/scala/dev/guardrail/generators/spi/ModuleLoadResult.scala
@@ -1,102 +1,104 @@
 package dev.guardrail.generators.spi
 
-import cats.Monad
+import cats.Applicative
 import cats.Monoid
 import cats.Semigroup
-import cats.StackSafeMonad
 import cats.data.NonEmptyList
 import cats.implicits._
 
 object ModuleLoadResult {
-  implicit val moduleLoadResultMonad: Monad[ModuleLoadResult] = new StackSafeMonad[ModuleLoadResult] {
-    def pure[A](x: A): ModuleLoadResult[A] = new ModuleLoadSuccess[A](Set.empty, Set.empty, Set.empty, x)
-
-    override def map[A, B](fa: ModuleLoadResult[A])(f: A => B) = fa match {
-      case success: ModuleLoadSuccess[_] => new ModuleLoadSuccess[B](success.attempted, success.consumed, success.missing, f(success.result))
-      case fail: ModuleLoadFailed        => fail
+  implicit val moduleLoadResultApplicative: Applicative[ModuleLoadResult] = new Applicative[ModuleLoadResult] {
+    def pure[A](x: A): ModuleLoadResult[A] = new ModuleLoadSuccess(Set.empty, Set.empty, x)
+    def ap[A, B](ff: ModuleLoadResult[A => B])(fa: ModuleLoadResult[A]): ModuleLoadResult[B] = ff match {
+      case f: ModuleLoadSuccess[A => B] =>
+        fa match {
+          case a: ModuleLoadSuccess[A] => new ModuleLoadSuccess[B](ff.attempted ++ fa.attempted, f.consumed ++ a.consumed, f.result(a.result))
+          case fail: ModuleLoadFailed  => fail
+        }
+      case ffail: ModuleLoadFailed =>
+        fa match {
+          case afail: ModuleLoadFailed => new ModuleLoadFailed(ffail.attempted ++ afail.attempted, ffail.missing ++ afail.missing)
+          case _                       => ffail
+        }
     }
-
-    def flatMap[A, B](fa: ModuleLoadResult[A])(f: A => ModuleLoadResult[B]) =
-      fa match {
-        case failed: ModuleLoadFailed => failed
-        case success: ModuleLoadSuccess[_] =>
-          f(success.result) match {
-            case next: ModuleLoadFailed =>
-              new ModuleLoadFailed(success.attempted ++ next.attempted, success.consumed ++ next.consumed, success.missing ++ next.missing)
-            case next: ModuleLoadSuccess[_] =>
-              new ModuleLoadSuccess[B](success.attempted ++ next.attempted, success.consumed ++ next.consumed, success.missing ++ next.missing, next.result)
-          }
-      }
   }
 
   implicit def moduleLoadResultSemigroup[A]: Monoid[ModuleLoadResult[A]] = new Monoid[ModuleLoadResult[A]] {
-    def empty: ModuleLoadResult[A] = new ModuleLoadFailed(Set.empty, Set.empty, Set.empty)
-    def combine(a: ModuleLoadResult[A], b: ModuleLoadResult[A]): ModuleLoadResult[A] = (a, b) match {
-      case (a: ModuleLoadSuccess[A], b: ModuleLoadSuccess[A]) =>
-        new ModuleLoadSuccess(a.attempted ++ b.attempted, a.consumed ++ b.consumed, a.missing ++ b.missing, a.result)
-      case (a: ModuleLoadFailed, b: ModuleLoadSuccess[A]) =>
-        new ModuleLoadSuccess(a.attempted ++ b.attempted, a.consumed ++ b.consumed, a.missing ++ b.missing, b.result)
-      case (a: ModuleLoadSuccess[A], b: ModuleLoadFailed) =>
-        new ModuleLoadSuccess(a.attempted ++ b.attempted, a.consumed ++ b.consumed, a.missing ++ b.missing, a.result)
-      case (a: ModuleLoadFailed, b: ModuleLoadFailed) => new ModuleLoadFailed(a.attempted ++ b.attempted, a.consumed ++ b.consumed, a.missing ++ b.missing)
-    }
+    def empty: ModuleLoadResult[A] = new ModuleLoadFailed(Set.empty, Set.empty)
+    def combine(a: ModuleLoadResult[A], b: ModuleLoadResult[A]): ModuleLoadResult[A] =
+      (a, b) match {
+        case (a: ModuleLoadSuccess[A], b: ModuleLoadSuccess[A]) =>
+          new ModuleLoadSuccess(a.attempted ++ b.attempted, a.consumed ++ b.consumed, a.result)
+        case (a: ModuleLoadFailed, b: ModuleLoadSuccess[A]) =>
+          new ModuleLoadSuccess(a.attempted ++ b.attempted, b.consumed, b.result)
+        case (a: ModuleLoadSuccess[A], b: ModuleLoadFailed) =>
+          new ModuleLoadSuccess(Set.empty, Set.empty, a.result)
+        case (a: ModuleLoadFailed, b: ModuleLoadFailed) =>
+          new ModuleLoadFailed(a.attempted ++ b.attempted, a.missing ++ b.missing)
+      }
   }
 
   private[this] def wrapper[Modules, Pivot](
       extract: String => Pivot
-  )(func: Pivot => ModuleLoadResult[Modules])(implicit ev1: Monoid[ModuleLoadResult[Modules]], ev2: Semigroup[Pivot]): Set[String] => Return[Modules] = {
-    modules =>
-      NonEmptyList
-        .fromList(modules.toList.sorted)
-        .fold(Monoid[ModuleLoadResult[Modules]].empty) { nel =>
-          func(nel.map(extract).reduce)
-        }
-        .toOption
+  )(
+      func: Pivot => ModuleLoadResult[Modules]
+  )(implicit ev1: Monoid[ModuleLoadResult[Modules]], ev2: Semigroup[Pivot]): Set[String] => Option[ModuleLoadResult[Modules]] = { modules =>
+    NonEmptyList
+      .fromList(modules.toList.sorted)
+      .map { nel =>
+        func(nel.map(extract).reduce)
+      }
   }
 
-  private[this] def work[A](a: Seq[String => Option[A]]): String => ModuleLoadResult[A] = { module =>
-    a.foldLeft[ModuleLoadResult[A]](new ModuleLoadFailed(Set(module), Set.empty, Set.empty)) { case (acc, next) =>
-      acc.combine(next(module).fold[ModuleLoadResult[A]](new ModuleLoadFailed(Set(module), Set.empty, Set.empty)) { res =>
-        new ModuleLoadSuccess[A](Set.empty, Set(module), Set.empty, res)
-      })
+  private[this] def work[A](a: (String, Seq[Map[String, A]])): String => ModuleLoadResult[A] = { module =>
+    val (label, mappings) = a
+    mappings.foldLeft[ModuleLoadResult[A]](new ModuleLoadFailed(Set.empty, Set(label))) { case (acc, next) =>
+      acc.combine(
+        next
+          .get(module)
+          .fold[ModuleLoadResult[A]](new ModuleLoadFailed(Set(module), Set(label))) { res =>
+            new ModuleLoadSuccess[A](Set(module), Set(module), res)
+          }
+      )
     }
   }
 
-  type Return[A] = Option[A]
-
-  def forProduct1[A, Z](a: Seq[String => Option[A]])(combine: A => Z): Set[String] => Return[Z] =
-    wrapper(module => work[A](a)(module))(_.map(combine))
-  def forProduct2[A, B, Z](a: Seq[String => Option[A]], b: Seq[String => Option[B]])(combine: (A, B) => Z): Set[String] => Return[Z] =
-    wrapper(module => (work[A](a)(module), work[B](b)(module)))(_.mapN(combine))
+  def forProduct1[A, Z](a: (String, Seq[Map[String, A]]))(combine: A => Z): Set[String] => ModuleLoadResult[Z] =
+    wrapper(module => work[A](a)(module))(_.map(combine)).map(_.getOrElse(new ModuleLoadFailed(Set.empty, Set(a._1))))
+  def forProduct2[A, B, Z](a: (String, Seq[Map[String, A]]), b: (String, Seq[Map[String, B]]))(combine: (A, B) => Z): Set[String] => ModuleLoadResult[Z] =
+    wrapper(module => (work[A](a)(module), work[B](b)(module)))(_.mapN(combine)).map(_.getOrElse(new ModuleLoadFailed(Set.empty, Set(a._1, b._1))))
   def forProduct3[A, B, C, Z](
-      a: Seq[String => Option[A]],
-      b: Seq[String => Option[B]],
-      c: Seq[String => Option[C]]
-  )(combine: (A, B, C) => Z): Set[String] => Return[Z] =
+      a: (String, Seq[Map[String, A]]),
+      b: (String, Seq[Map[String, B]]),
+      c: (String, Seq[Map[String, C]])
+  )(combine: (A, B, C) => Z): Set[String] => ModuleLoadResult[Z] =
     wrapper(module => (work[A](a)(module), work[B](b)(module), work[C](c)(module)))(_.mapN(combine))
+      .map(_.getOrElse(new ModuleLoadFailed(Set.empty, Set(a._1, b._1, c._1))))
   def forProduct4[A, B, C, D, Z](
-      a: Seq[String => Option[A]],
-      b: Seq[String => Option[B]],
-      c: Seq[String => Option[C]],
-      d: Seq[String => Option[D]]
-  )(combine: (A, B, C, D) => Z): Set[String] => Return[Z] =
+      a: (String, Seq[Map[String, A]]),
+      b: (String, Seq[Map[String, B]]),
+      c: (String, Seq[Map[String, C]]),
+      d: (String, Seq[Map[String, D]])
+  )(combine: (A, B, C, D) => Z): Set[String] => ModuleLoadResult[Z] =
     wrapper(module => (work[A](a)(module), work[B](b)(module), work[C](c)(module), work[D](d)(module)))(_.mapN(combine))
+      .map(_.getOrElse(new ModuleLoadFailed(Set.empty, Set(a._1, b._1, c._1, d._1))))
   def forProduct5[A, B, C, D, E, Z](
-      a: Seq[String => Option[A]],
-      b: Seq[String => Option[B]],
-      c: Seq[String => Option[C]],
-      d: Seq[String => Option[D]],
-      e: Seq[String => Option[E]]
-  )(combine: (A, B, C, D, E) => Z): Set[String] => Return[Z] =
+      a: (String, Seq[Map[String, A]]),
+      b: (String, Seq[Map[String, B]]),
+      c: (String, Seq[Map[String, C]]),
+      d: (String, Seq[Map[String, D]]),
+      e: (String, Seq[Map[String, E]])
+  )(combine: (A, B, C, D, E) => Z): Set[String] => ModuleLoadResult[Z] =
     wrapper(module => (work[A](a)(module), work[B](b)(module), work[C](c)(module), work[D](d)(module), work[E](e)(module)))(_.mapN(combine))
+      .map(_.getOrElse(new ModuleLoadFailed(Set.empty, Set(a._1, b._1, c._1, d._1, e._1))))
   def forProduct6[A, B, C, D, E, F, Z](
-      a: Seq[String => Option[A]],
-      b: Seq[String => Option[B]],
-      c: Seq[String => Option[C]],
-      d: Seq[String => Option[D]],
-      e: Seq[String => Option[E]],
-      f: Seq[String => Option[F]]
-  )(combine: (A, B, C, D, E, F) => Z): Set[String] => Return[Z] =
+      a: (String, Seq[Map[String, A]]),
+      b: (String, Seq[Map[String, B]]),
+      c: (String, Seq[Map[String, C]]),
+      d: (String, Seq[Map[String, D]]),
+      e: (String, Seq[Map[String, E]]),
+      f: (String, Seq[Map[String, F]])
+  )(combine: (A, B, C, D, E, F) => Z): Set[String] => ModuleLoadResult[Z] =
     wrapper(module =>
       (
         work[A](a)(module),
@@ -106,7 +108,7 @@ object ModuleLoadResult {
         work[E](e)(module),
         work[F](f)(module)
       )
-    )(_.mapN(combine))
+    )(_.mapN(combine)).map(_.getOrElse(new ModuleLoadFailed(Set.empty, Set(a._1, b._1, c._1, d._1, e._1, f._1))))
 
   def buildFrom[Modules, Extracted[_]](extract: String => Extracted[Modules])(implicit ev1: Monoid[Extracted[Modules]]): Set[String] => Extracted[Modules] = {
     parameters =>
@@ -118,27 +120,14 @@ object ModuleLoadResult {
   }
 }
 
-sealed abstract class ModuleLoadResult[+A](val attempted: Set[String], val consumed: Set[String], val missing: Set[String]) {
-  def map[B](f: A => B): ModuleLoadResult[B]
-  def flatMap[B](f: A => ModuleLoadResult[B]): ModuleLoadResult[B]
-  def toOption: Option[A]
+// Parameters:
+// - attempted: All mapping keys for the current stage
+sealed abstract class ModuleLoadResult[+A](val attempted: Set[String])
+
+class ModuleLoadFailed(attempted: Set[String], val missing: Set[String]) extends ModuleLoadResult[Nothing](attempted) {
+  override def toString(): String = s"ModuleLoadFailed($attempted, $missing)"
 }
 
-class ModuleLoadFailed(attempted: Set[String], consumed: Set[String], missing: Set[String]) extends ModuleLoadResult[Nothing](attempted, consumed, missing) {
-  def map[B](f: Nothing => B)                       = this
-  def flatMap[B](f: Nothing => ModuleLoadResult[B]) = this
-  def toOption: Option[Nothing]                     = None
-  override def toString(): String                   = s"ModuleLoadFailed($attempted, $consumed, $missing)"
-}
-
-class ModuleLoadSuccess[A](attempted: Set[String], consumed: Set[String], missing: Set[String], val result: A)
-    extends ModuleLoadResult[A](attempted, consumed, missing) {
-  def map[B](f: A => B) = new ModuleLoadSuccess[B](attempted, consumed, missing, f(result))
-  def flatMap[B](f: A => ModuleLoadResult[B]) =
-    f(result) match {
-      case next: ModuleLoadFailed     => new ModuleLoadFailed(attempted ++ next.attempted, consumed ++ next.consumed, missing ++ next.missing)
-      case next: ModuleLoadSuccess[_] => new ModuleLoadSuccess[B](attempted ++ next.attempted, consumed ++ next.consumed, missing ++ next.missing, next.result)
-    }
-  def toOption: Option[A]         = Some(result)
-  override def toString(): String = s"ModuleLoadSuccess($attempted, $consumed, $missing, $result)"
+class ModuleLoadSuccess[A](attempted: Set[String], val consumed: Set[String], val result: A) extends ModuleLoadResult[A](attempted) {
+  override def toString(): String = s"ModuleLoadSuccess($attempted, $consumed, $result)"
 }
--- modules/core/src/test/scala/dev/guardrail/generators/spi/ModuleLoadResultSpec.scala
+++ modules/core/src/test/scala/dev/guardrail/generators/spi/ModuleLoadResultSpec.scala
@@ -1,46 +1,69 @@
 package dev.guardrail.generators.spi
 
+import cats.implicits._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.OptionValues
-import scala.util.Try
 
 class ModuleLoadResultSpec extends AnyFunSuite with Matchers with OptionValues {
-  test("ModuleLoadResult.forProduct1") {
-    val extractor = ModuleLoadResult.forProduct1( Seq((x: String) => Try(x.toBoolean).toOption)) { b => b }
-
-    extractor(Set("foop", "true")).value shouldBe true
+  private def getMissing[A](value: ModuleLoadResult[A]): Set[String] = value match {
+    case x: ModuleLoadFailed => x.missing
+    case _                   => fail("Expected failure")
   }
 
-  test("ModuleLoadResult.forProduct2") {
+  private def getConsumed[A](value: ModuleLoadResult[A]): Set[String] = value match {
+    case x: ModuleLoadSuccess[A] => x.consumed
+    case _                       => fail("Expected success")
+  }
+
+  test("Combine Tuple2") {
+    val a: ModuleLoadResult[Long]   = new ModuleLoadFailed(Set.empty, Set("foo"))
+    val b: ModuleLoadResult[String] = new ModuleLoadFailed(Set.empty, Set("bar"))
+    val c: ModuleLoadResult[String] = new ModuleLoadSuccess(Set.empty, Set("c"), "woo")
+    val d: ModuleLoadResult[String] = new ModuleLoadSuccess(Set.empty, Set("d"), "woo")
+
+    getMissing((a, b).mapN(Tuple2.apply)) should contain theSameElementsAs (Set("foo", "bar"))
+    getMissing((a, c).mapN(Tuple2.apply)) should contain theSameElementsAs (Set("foo"))
+    getMissing((c, a).mapN(Tuple2.apply)) should contain theSameElementsAs (Set("foo"))
+
+    getConsumed((c, d).mapN(Tuple2.apply)) should contain theSameElementsAs (Set("c", "d"))
+  }
+
+  test("ModuleLoadResult forProduct2") {
+    val a: Map[String, Boolean] = Map("false" -> false)
+    val b: Map[String, Long]    = Map("1" -> 1L)
     val extractor = ModuleLoadResult.forProduct2(
-      Seq((x: String) => Try(x.toBoolean).toOption),
-      Seq((x: String) => Try(x.toLong).toOption)
-    ) { (a, b) => (a, b) }
+      ("A Component", Seq(a)),
+      ("B Component", Seq(b))
+    )((a, b) => (a, b))
 
-    extractor(Set("foop", "true", "5")).value shouldBe ((true, 5L))
+    getMissing(extractor(Set("a", "b", "c", "d"))) should contain theSameElementsAs (Set("A Component", "B Component"))
   }
 
-  test("ModuleLoadResult.forProduct3") {
-    {
-      val extractor = ModuleLoadResult.forProduct3(
-        Seq((x: String) => Try(x.toBoolean).toOption),
-        Seq((x: String) => Try(x.toLong).toOption),
-        Seq((x: String) => Option(x).filter(_.nonEmpty)), // Expect the first string to be found
-      ) { (a, b, c) => (a, b, c) }
+  test("ModuleLoadResult forProduct6") {
+    val a: Map[String, Boolean] = Map("false" -> false)
+    val b: Map[String, Long]    = Map("1" -> 1L)
+    val c: Map[String, Int]     = Map("1" -> 2)
+    val d: Map[String, Char]    = Map("c" -> 'c')
+    val e: Map[String, Float]   = Map("pi" -> 3.14f)
+    val f: Map[String, String]  = Map("foo" -> "foo")
+    val extractor = ModuleLoadResult.forProduct6(
+      ("A Component", Seq(a)),
+      ("B Component", Seq(b)),
+      ("C Component", Seq(c)),
+      ("D Component", Seq(d)),
+      ("E Component", Seq(e)),
+      ("F Component", Seq(f))
+    )((a, b, c, d, e, f) => (a, b, c, d, e, f))
 
-      extractor(Set("foop", "true", "5", "boop")).value shouldBe ((true, 5L, "5"))
-    }
-
-    {
-      val extractor = ModuleLoadResult.forProduct3(
-        Seq((x: String) => Try(x.toBoolean).toOption),
-        Seq((x: String) => Try(x.toLong).toOption),
-        Seq((x: String) => Option(x).filter(_ == "foop")),
-      ) { (a, b, c) => (a, b, c) }
-
-      extractor(Set("foop", "true", "5", "boop")).value shouldBe ((true, 5L, "foop"))
-    }
+    getMissing(extractor(Set.empty)) should contain theSameElementsAs (Set(
+      "A Component",
+      "B Component",
+      "C Component",
+      "D Component",
+      "E Component",
+      "F Component"
+    ))
   }
 
 }
