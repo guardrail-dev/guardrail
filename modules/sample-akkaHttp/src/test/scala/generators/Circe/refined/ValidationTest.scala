0104c527a5ed3bc8b9167f472fcea41362984fe1
Devon Stewart <blast@hardchee.se>
2022-07-09 13:38:05 -0700

    Resolving deprecation warnings

--- modules/sample-akkaHttp/src/test/scala/generators/Circe/refined/ValidationTest.scala
+++ modules/sample-akkaHttp/src/test/scala/generators/Circe/refined/ValidationTest.scala
@@ -11,47 +11,47 @@ class ValidationTest extends AnyFreeSpec with Matchers with EitherValues {
 
   "maximum validation" - {
     "should be inclusive" in {
-      Validated.decodeValidated(parse("""{ "max_validation": 100 }""").right.get.hcursor).right.value shouldBe Validated(Some(100), None, None)
+      Validated.decodeValidated(parse("""{ "max_validation": 100 }""").value.hcursor).value shouldBe Validated(Some(100), None, None)
     }
     "should succeed" in {
-      Validated.decodeValidated(parse("""{ "max_validation": 10 }""").right.get.hcursor).right.value shouldBe Validated(Some(10), None, None)
+      Validated.decodeValidated(parse("""{ "max_validation": 10 }""").value.hcursor).value shouldBe Validated(Some(10), None, None)
     }
     "should fail on incorrect input" in {
-      Validated.decodeValidated(parse("""{ "max_validation": 101 }""").right.get.hcursor).isLeft shouldBe true
+      Validated.decodeValidated(parse("""{ "max_validation": 101 }""").value.hcursor).isLeft shouldBe true
     }
   }
 
   "minimum validation" - {
     "should be inclusive" in {
-      Validated.decodeValidated(parse("""{ "min_validation": 1 }""").right.get.hcursor).right.value shouldBe Validated(None, Some(1), None)
+      Validated.decodeValidated(parse("""{ "min_validation": 1 }""").value.hcursor).value shouldBe Validated(None, Some(1), None)
     }
     "should succeed" in {
-      Validated.decodeValidated(parse("""{ "min_validation": 10 }""").right.get.hcursor).right.value shouldBe Validated(None, Some(10), None)
+      Validated.decodeValidated(parse("""{ "min_validation": 10 }""").value.hcursor).value shouldBe Validated(None, Some(10), None)
     }
     "should fail on incorrect input" in {
-      Validated.decodeValidated(parse("""{ "min_validation": 0 }""").right.get.hcursor).isLeft shouldBe true
+      Validated.decodeValidated(parse("""{ "min_validation": 0 }""").value.hcursor).isLeft shouldBe true
     }
   }
 
   "range validation" - {
     "should succeed within the range" in {
-      Validated.decodeValidated(parse("""{ "range_validation": 10 }""").right.get.hcursor).right.value shouldBe Validated(None, None, Some(10))
+      Validated.decodeValidated(parse("""{ "range_validation": 10 }""").value.hcursor).value shouldBe Validated(None, None, Some(10))
     }
 
     "should be inclusive - higher bound" in {
-      Validated.decodeValidated(parse("""{ "range_validation": 100 }""").right.get.hcursor).right.value shouldBe Validated(None, None, Some(100))
+      Validated.decodeValidated(parse("""{ "range_validation": 100 }""").value.hcursor).value shouldBe Validated(None, None, Some(100))
     }
 
     "should be inclusive - lower bound" in {
-      Validated.decodeValidated(parse("""{ "range_validation": 0 }""").right.get.hcursor).right.value shouldBe Validated(None, None, Some(0))
+      Validated.decodeValidated(parse("""{ "range_validation": 0 }""").value.hcursor).value shouldBe Validated(None, None, Some(0))
     }
 
     "should fail on incorrect input - lower bound" in {
-      Validated.decodeValidated(parse("""{ "range_validation": -1 }""").right.get.hcursor).isLeft shouldBe true
+      Validated.decodeValidated(parse("""{ "range_validation": -1 }""").value.hcursor).isLeft shouldBe true
     }
 
     "should fail on incorrect input - higher bound" in {
-      Validated.decodeValidated(parse("""{ "range_validation": 101 }""").right.get.hcursor).isLeft shouldBe true
+      Validated.decodeValidated(parse("""{ "range_validation": 101 }""").value.hcursor).isLeft shouldBe true
     }
 
   }
