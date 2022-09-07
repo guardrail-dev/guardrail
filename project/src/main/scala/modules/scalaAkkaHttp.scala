d889d08a65b0687d4c3d9a2f2a90efc46844cda6
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 16:25:44 +0000

    Update akka-http, akka-http-testkit to 10.2.10

--- project/src/main/scala/modules/scalaAkkaHttp.scala
+++ project/src/main/scala/modules/scalaAkkaHttp.scala
@@ -7,7 +7,7 @@ import sbt.Keys._
 import wartremover.WartRemover.autoImport._
 
 object scalaAkkaHttp {
-  val akkaHttpVersion        = "10.2.9"
+  val akkaHttpVersion        = "10.2.10"
   val akkaVersion            = "2.6.20"
   val catsVersion            = "2.8.0"
   val circeVersion           = "0.14.3"
