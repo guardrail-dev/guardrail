3307c66318011d331fa39ccad33b7d854f3d6982
Scala Steward <scala_steward@virtuslab.com>
2022-11-14 20:52:51 +0000

    Update cats-effect to 3.4.0

--- project/src/main/scala/modules/scalaHttp4s.scala
+++ project/src/main/scala/modules/scalaHttp4s.scala
@@ -37,7 +37,7 @@ object scalaHttp4s {
   }
 
   val dependencies = {
-    val catsEffectVersion      = "3.3.14"
+    val catsEffectVersion      = "3.4.0"
     val http4sVersion          = "0.23.16"
     val http4sBlazeVersion     = "0.23.12"
     val refinedVersion         = "0.10.1"
