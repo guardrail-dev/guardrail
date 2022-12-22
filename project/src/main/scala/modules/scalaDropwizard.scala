c72c81c051adada30d76998e7e4fb9a04f95fa10
Scala Steward <scala_steward@virtuslab.com>
2022-12-22 15:35:36 +0000

    Update jersey-test-framework-provider-grizzly2 to 2.38

--- project/src/main/scala/modules/scalaDropwizard.scala
+++ project/src/main/scala/modules/scalaDropwizard.scala
@@ -12,7 +12,7 @@ object scalaDropwizard {
   val jacksonVersion         = "2.13.4"
   val javaxAnnotationVersion = "1.3.2"
   val jaxbApiVersion         = "2.3.1"
-  val jerseyVersion          = "2.25.1"
+  val jerseyVersion          = "2.38"
   val scalatestVersion       = "3.2.13"
 
   val dependencies = Seq(
