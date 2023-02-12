8d3506d319298cae466b643200f4a7fc42f5dd8e
Scala Steward <scala_steward@virtuslab.com>
2023-02-12 15:50:59 +0000

    Update scalameta, semanticdb-scalac to 4.7.4

--- project/src/main/scala/modules/scalaSupport.scala
+++ project/src/main/scala/modules/scalaSupport.scala
@@ -9,6 +9,6 @@ object scalaSupport {
   val project =
     commonModule("scala-support")
       .settings(
-        libraryDependencies += "org.scalameta" %% "scalameta" % "4.5.13"
+        libraryDependencies += "org.scalameta" %% "scalameta" % "4.7.4"
       )
 }
