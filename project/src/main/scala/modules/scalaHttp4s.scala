58005e158e141e2bb536bd1a50bac4917734b98d
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 22:06:41 +0000

    Update http4s-circe, http4s-dsl to 0.23.19

--- project/src/main/scala/modules/scalaHttp4s.scala
+++ project/src/main/scala/modules/scalaHttp4s.scala
@@ -38,7 +38,7 @@ object scalaHttp4s {
 
   val dependencies = {
     val catsEffectVersion      = "3.4.10"
-    val http4sVersion          = "0.23.18"
+    val http4sVersion          = "0.23.19"
     val http4sBlazeVersion     = "0.23.14"
     val refinedVersion         = "0.10.1"
 
