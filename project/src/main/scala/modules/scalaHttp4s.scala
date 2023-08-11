7a8d2bacda75cb6590a5cbbbe78c27e2197248dc
Scala Steward <scala_steward@virtuslab.com>
2023-08-11 17:00:47 +0000

    Update refined, refined-cats to 0.11.0

--- project/src/main/scala/modules/scalaHttp4s.scala
+++ project/src/main/scala/modules/scalaHttp4s.scala
@@ -15,7 +15,7 @@ object scalaHttp4s {
   val dependenciesV0_22 = {
     val catsEffectVersion      = "2.5.4"
     val http4sVersion          = "0.22.7"
-    val refinedVersion         = "0.10.3"
+    val refinedVersion         = "0.11.0"
 
     Seq(
       "javax.annotation" %  "javax.annotation-api"  % javaxAnnotationVersion, // for jdk11
@@ -40,7 +40,7 @@ object scalaHttp4s {
     val catsEffectVersion      = "3.4.11"
     val http4sVersion          = "0.23.18"
     val http4sBlazeVersion     = "0.23.14"
-    val refinedVersion         = "0.10.3"
+    val refinedVersion         = "0.11.0"
 
     Seq(
       "javax.annotation" %  "javax.annotation-api"  % javaxAnnotationVersion, // for jdk11
