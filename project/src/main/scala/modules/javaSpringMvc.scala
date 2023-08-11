a28c92c617a2214cb8985fdb71355a5faa291243
Scala Steward <scala_steward@virtuslab.com>
2023-08-11 17:01:47 +0000

    Revert commit(s) af498de1

--- project/src/main/scala/modules/javaSpringMvc.scala
+++ project/src/main/scala/modules/javaSpringMvc.scala
@@ -7,7 +7,7 @@ import sbt.Keys._
 
 object javaSpringMvc {
   val javaxAnnotationVersion = "1.3.2"
-  val scalatestVersion       = "3.2.16"
+  val scalatestVersion       = "3.2.13"
   val springBootVersion      = "2.7.11"
 
   val dependencies = Seq(
