59b0ff08b744bd3d8e44d4131291a660ad9789c0
Devon Stewart <blast@hardchee.se>
2023-10-09 19:43:56 -0700

    Removing deprecated accessors for Tracker

--- modules/core/src/main/scala/dev/guardrail/core/Tracker.scala
+++ modules/core/src/main/scala/dev/guardrail/core/Tracker.scala
@@ -131,13 +131,7 @@ trait HighPriorityTrackerSyntax extends LowPriorityTrackerSyntax {
 
     def showHistory: String = tracker.history.mkString("")
 
-    @deprecated("Tracker.get will be removed once the migration has been completed. Please use the fold/traverse combinators to get values out.", "0.0.0")
-    def get: A = tracker.get
-
     def unwrapTracker: A = tracker.get
-
-    @deprecated("Tracker.get will be removed once the migration has been completed. Please use the fold/traverse combinators to get values out.", "0.0.0")
-    def history: Vector[String] = tracker.history
 
     def forceConvince[C](implicit ev: Tracker.Convincer[Option[A], C]): Tracker[C] =
       tracker.map(x => ev(Option(x)))
