575414eb4740d8cdc30ea677d7f51e8d4730a6a1
Scala Steward <scala_steward@virtuslab.com>
2023-10-18 18:04:28 +0000

    Update swagger-parser to 2.1.18

--- project/src/main/scala/modules/core.scala
+++ project/src/main/scala/modules/core.scala
@@ -12,7 +12,7 @@ object core {
     commonModule("core")
       .settings(
         libraryDependencies ++= Seq(
-          "io.swagger.parser.v3"        % "swagger-parser"                % "2.1.16",
+          "io.swagger.parser.v3"        % "swagger-parser"                % "2.1.18",
         ) ++ Seq(
           "org.scala-lang.modules"      %% "scala-collection-compat"      % "2.11.0",
           "org.tpolecat"                %% "atto-core"                    % "0.9.5",
