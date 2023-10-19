10a08f1ce212336516ef29e67336b8c2f225ad01
Devon Stewart <blast@hardchee.se>
2023-10-18 19:38:32 -0700

    Use the search API to discover all labels in play

--- support/current-pr-labels.sh
+++ support/current-pr-labels.sh
@@ -2,15 +2,33 @@
 
 set -e
 
+module_name="$1"
+
+gh_api() {
+  path="$1"; shift
+  curl \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    "$@" \
+    "https://api.github.com/$path"
+}
+
 fetch_pr() {
   pr_number="$1"
   cache="$2"
   echo "Fetching labels for current PR (${pr_number})" >&2
-  curl \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    -o "$cache" \
-    "https://api.github.com/repos/guardrail-dev/guardrail/pulls/${pr_number}"
+  gh_api "repos/guardrail-dev/guardrail/pulls/${pr_number}" -o "$cache"
+}
+
+labels_since_last_release() {
+  module_name="$1"; shift || die 'Missing module_name'
+  cache="$1"; shift || die 'Missing cache target'
+
+  latest_tag="$(git tag | grep "^$module_name-v[0-9]" | tail -n 1)"
+  module_released_on="$(git show "${latest_tag}" --format=%cI)"
+  gh_api "search/issues?q=repo:guardrail-dev/guardrail+type:pr+is:merged+closed:>${module_released_on}" \
+    | jq -cM '{labels: (.items | map(.labels) | flatten | map(.name) | unique | map({ name: . })) }' \
+    > "$cache"
 }
 
 cachedir=target/github
@@ -21,6 +39,8 @@ if [ -n "$GITHUB_EVENT_PATH" ]; then
 
   if [ -f "$cache" ]; then
     echo "Using PR labels from $cache" >&2
+  elif [ -n "$module_name" ]; then
+    labels_since_last_release "$module_name" "$cache"
   elif [ "$pr_number" != "null" ]; then
     fetch_pr "$pr_number" "$cache"
   else
