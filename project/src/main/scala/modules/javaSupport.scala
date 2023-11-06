2e785d40c55a5011188ec9bb28dd5ebaf5fe1bcc
Scala Steward <scala_steward@virtuslab.com>
2023-11-06 14:12:24 +0000

    Update javaparser-symbol-solver-core to 3.25.6

--- project/src/main/scala/modules/javaSupport.scala
+++ project/src/main/scala/modules/javaSupport.scala
@@ -35,6 +35,6 @@ object javaSupport {
     commonModule("java-support")
       .settings(
         libraryDependencies ++= eclipseFormatterDependencies,
-        libraryDependencies += "com.github.javaparser" % "javaparser-symbol-solver-core" % "3.25.5"
+        libraryDependencies += "com.github.javaparser" % "javaparser-symbol-solver-core" % "3.25.6"
       )
 }
