f459acb0661311fd2f4d8eceba3f8fd42afe8583
Scala Steward <scala_steward@virtuslab.com>
2024-02-21 20:37:59 +0000

    Update scalatest to 3.2.18

--- project/src/main/scala/modules/javaSpringMvc.scala
+++ project/src/main/scala/modules/javaSpringMvc.scala
@@ -7,7 +7,7 @@ import sbt.Keys._
 
 object javaSpringMvc {
   val javaxAnnotationVersion = "1.3.2"
-  val scalatestVersion       = "3.2.17"
+  val scalatestVersion       = "3.2.18"
   val springBootVersion      = "2.7.18"
 
   val dependencies = Seq(
