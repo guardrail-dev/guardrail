5419667e7649bc88fe9a48058edbce22760626b8
Scala Steward <scala_steward@virtuslab.com>
2024-03-10 14:42:54 +0000

    Update jackson-annotations, jackson-core, ... to 2.16.2

--- project/src/main/scala/modules/scalaAkkaHttp.scala
+++ project/src/main/scala/modules/scalaAkkaHttp.scala
@@ -12,7 +12,7 @@ object scalaAkkaHttp {
   val catsVersion            = "2.10.0"
   val circeVersion           = "0.14.6"
   val hibernateVersion       = "6.2.5.Final"
-  val jacksonVersion         = "2.16.0"
+  val jacksonVersion         = "2.16.2"
   val javaxAnnotationVersion = "1.3.2"
   val javaxElVersion         = "3.0.0"
   val jaxbApiVersion         = "2.3.1"
