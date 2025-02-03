049bdd233aa7c270279b0baf15d6bd75a7b6f24f
Scala Steward <scala_steward@virtuslab.com>
2025-02-03 22:59:20 +0000

    Update vavr to 0.10.6

--- project/src/main/scala/modules/javaDropwizard.scala
+++ project/src/main/scala/modules/javaDropwizard.scala
@@ -13,7 +13,7 @@ object javaDropwizard {
   val jaxbApiVersion         = "2.3.1"
   val jerseyVersion          = "2.25.1"
   val scalatestVersion       = "3.2.18"
-  val vavrVersion            = "0.10.3"
+  val vavrVersion            = "0.10.6"
 
   val dependencies = Seq(
     "javax.annotation"           %  "javax.annotation-api"   % javaxAnnotationVersion, // for jdk11
