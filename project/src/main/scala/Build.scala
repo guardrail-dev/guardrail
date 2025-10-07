496c86f86844b78d707e647cfb95c8263e127087
Scala Steward <scala_steward@virtuslab.com>
2025-10-07 15:30:57 +0000

    Update scala-collection-compat to 2.14.0

--- project/src/main/scala/Build.scala
+++ project/src/main/scala/Build.scala
@@ -47,7 +47,7 @@ object Build {
     Project(s"sample-${name}", file(s"modules/sample-${name}"))
       .settings(commonSettings)
       .settings(codegenSettings)
-      .settings(libraryDependencies += "org.scala-lang.modules" %% "scala-collection-compat" % "2.11.0")
+      .settings(libraryDependencies += "org.scala-lang.modules" %% "scala-collection-compat" % "2.14.0")
       .settings(
         libraryDependencies ++= extraLibraryDependencies,
         Compile / unmanagedSourceDirectories += baseDirectory.value / "target" / "generated",
--- project/src/main/scala/modules/core.scala
+++ project/src/main/scala/modules/core.scala
@@ -105,7 +105,7 @@ object core {
         libraryDependencies ++= Seq(
           "io.swagger.parser.v3"        % "swagger-parser"                % "2.1.45",
         ) ++ Seq(
-          "org.scala-lang.modules"      %% "scala-collection-compat"      % "2.11.0",
+          "org.scala-lang.modules"      %% "scala-collection-compat"      % "2.14.0",
           "org.tpolecat"                %% "atto-core"                    % "0.9.5",
           "org.typelevel"               %% "cats-core"                    % catsVersion,
           "org.typelevel"               %% "cats-kernel"                  % catsVersion,
